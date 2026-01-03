9dc7847a31f4e7cf790aaa22f63d4934674f78eb
jungji777 <137734822+jungji777@users.noreply.github.com>
2026-01-02 23:35:31 -0600

    Updated Certifications.tsx

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -39,7 +39,7 @@ const Certifications = () => {
     {
       icon: <Trophy className="w-6 h-6" />,
       image:
-        "https://res.cloudinary.com/df5jie5rz/image/upload/v1767346571/aws-amazonEMR_n7x8bt.pdf",
+        "https://res.cloudinary.com/df5jie5rz/image/upload/v1767414031/Captura_de_pantalla_2026-01-02_a_la_s_19.27.00_zjltmf.png",
       title: "Amazon EMR",
       description: "Certified Professional",
       date: "2025",
